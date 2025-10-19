695f51907c68358fbf35eef1fe9e685e666a56f0
Arun Balekai <arunbalekai25@gmail.com>
2025-10-19 17:12:41 +0530

    Added TiDB Cloud SSL CA certificate abd  updated DB connection

--- db_connect.php
+++ db_connect.php
@@ -1,11 +1,20 @@
 <?php
-$host = 'localhost';
-$db   = 'traveltrack_db';
-$user = 'root';
-$pass = ''; // XAMPP default
-$conn = new mysqli($host, $user, $pass, $db);
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+$host = "gateway01.ap-southeast-1.prod.aws.tidbcloud.com";
+$port = 4000;
+$user = "yhQSbqlWsM4HhLAm.root";
+$pass = "RP7JZZZH2snJFp3j";
+$db   = "test";
+$ssl_ca = __DIR__ . "/ca.pem"; // path to your CA file
+
+$conn = mysqli_init();
+
+// Set SSL options
+mysqli_ssl_set($conn, NULL, NULL, $ssl_ca, NULL, NULL);
+
+// Establish a secure connection
+if (!mysqli_real_connect($conn, $host, $user, $pass, $db, $port, NULL, MYSQLI_CLIENT_SSL)) {
+    die("Connection failed: " . mysqli_connect_error());
 }
+
 $conn->set_charset("utf8mb4");
 ?>
